f97695d191a241b659170e25b02abc22e1c9db99
Muhammad Iqbal CR <muh.iqbal.cr@gmail.com>
2018-05-22 17:56:24 +0700

    wifi: Removed roaming scenario on StaWifiMac test

--- src/wifi/test/sta-wifi-mac-test.cc
+++ src/wifi/test/sta-wifi-mac-test.cc
@@ -18,29 +18,19 @@
  * Authors: Muhammad Iqbal CR <muh.iqbal.cr@gmail.com>
  */
 
+#include "ns3/boolean.h"
 #include "ns3/config.h"
-#include "ns3/constant-position-mobility-model.h"
-#include "ns3/constant-velocity-mobility-model.h"
 #include "ns3/mac48-address.h"
-#include "ns3/mobility-model.h"
+#include "ns3/mobility-helper.h"
 #include "ns3/log.h"
 #include "ns3/node-container.h"
-#include "ns3/object-factory.h"
 #include "ns3/pointer.h"
-#include "ns3/propagation-delay-model.h"
-#include "ns3/propagation-loss-model.h"
 #include "ns3/simulator.h"
-#include "ns3/string.h"
 #include "ns3/test.h"
-#include "ns3/yans-error-rate-model.h"
-#include "ns3/yans-wifi-channel.h"
-#include "ns3/yans-wifi-phy.h"
-#include "ns3/wifi-mac.h"
-#include "ns3/wifi-net-device.h"
-#include "ns3/wifi-remote-station-manager.h"
-#include <string>
-#include <tuple>
-#include <vector>
+#include "ns3/yans-wifi-helper.h"
+#include "ns3/wifi-helper.h"
+#include "ns3/wifi-mac-header.h"
+#include "ns3/wifi-mac-helper.h"
 
 using namespace ns3;
 
@@ -63,48 +53,28 @@ public:
 
 private:
   /**
-   * A tuple of {STA node id, associated MAC address}
-   */
-  typedef std::tuple<uint32_t, Mac48Address> AssocTuple;
-  std::vector<AssocTuple> m_assocLog; ///< vector containing assoc log on STA assoc
-  Ptr<ConstantVelocityMobilityModel> m_mobileStaMobility; ///< variable to contain mobile sta mobility model
-
-  /**
-   * Function to get node ID from context string
-   * \param context context string
-   */
-  uint32_t ContextToNodeId(std::string context);
-  /**
-   * Function to create node
-   * \param wifiMacTypeId type ID string
-   * \param channel channel model
-   * \param mobility mobility model
-   * \param channelNumber channel number
-   */
-  Ptr<Node> CreateNode(std::string wifiMacTypeId, Ptr<YansWifiChannel> channel, Ptr<MobilityModel> mobility, uint8_t channelNumber);
-  /**
    * Callback function on STA assoc event
    * \param context context string
-   * \param bssidAddr the associated AP's MAC address
+   * \param bssid the associated AP's bssid
    */
-  void AssocCallback (std::string context, Mac48Address bssidAddr);
+  void AssocCallback (std::string context, Mac48Address bssid);
   /**
-   * Function to run test with static STA
+   * Callback function on STA RX event
+   * \param context context string
+   * \param p the received packet
    */
-  void RunStaticStaTest();
+  void RxCallback (std::string context, Ptr<const Packet> p);
   /**
-   * Function to run test with mobile STA
+   * Turn beacon generation on the AP node
+   * \param apNode the AP node
    */
-  void RunMobileStaTest();
-  /**
-   * Function to change STA velocity on mobile STA test
-   * \param vector speed vector
-   */
-  void SetStaVelocity(Vector vector);
+  void TurnBeaconGenerationOn (Ptr<Node> apNode);
+
+  Mac48Address m_associatedApBssid;
 };
 
 StaWifiMacTestCase::StaWifiMacTestCase ()
-  : TestCase ("Test case for StaWifiMac with static STA")
+  : TestCase ("Test case for StaWifiMac")
 {
 }
 
@@ -112,143 +82,87 @@ StaWifiMacTestCase::~StaWifiMacTestCase ()
 {
 }
 
-uint32_t
-StaWifiMacTestCase::ContextToNodeId (std::string context)
+void
+StaWifiMacTestCase::AssocCallback (std::string context, Mac48Address bssid)
 {
-  std::string sub = context.substr (10);  // skip "/NodeList/"
-  uint32_t pos = sub.find ("/Device");
-  return atoi (sub.substr (0, pos).c_str ());
-}
-
-Ptr<Node>
-StaWifiMacTestCase::CreateNode (std::string wifiMacTypeId, Ptr<YansWifiChannel> channel, Ptr<MobilityModel> mobility, uint8_t channelNumber)
-{
-	Ptr<Node> node = CreateObject<Node> ();
-  Ptr<WifiNetDevice> dev = CreateObject<WifiNetDevice> ();
-  ObjectFactory mac;
-  mac.SetTypeId (wifiMacTypeId);
-  Ptr<WifiMac> wifiMac = mac.Create<WifiMac> ();
-  wifiMac->ConfigureStandard (WIFI_PHY_STANDARD_80211n_2_4GHZ);
-
-  Ptr<ErrorRateModel> error = CreateObject<YansErrorRateModel> ();
-  Ptr<YansWifiPhy> phy = CreateObject<YansWifiPhy> ();
-  phy->SetErrorRateModel (error);
-  phy->SetChannel (channel);
-  phy->SetMobility (mobility);
-  phy->SetDevice (dev);
-  phy->ConfigureStandard (WIFI_PHY_STANDARD_80211n_2_4GHZ);
-  phy->SetChannelNumber (channelNumber);
-  phy->SetChannelWidth (20);
-
-  wifiMac->SetAddress (Mac48Address::Allocate ());
-  dev->SetMac (wifiMac);
-  dev->SetPhy (phy);
-  ObjectFactory manager;
-  manager.SetTypeId ("ns3::ConstantRateWifiManager");
-  dev->SetRemoteStationManager (manager.Create<WifiRemoteStationManager> ());
-  node->AddDevice (dev);
-
-  return node;
+  NS_LOG_DEBUG ("STA has associated with " << bssid);
+  m_associatedApBssid = bssid;
 }
 
 void
-StaWifiMacTestCase::AssocCallback (std::string context, Mac48Address bssidAddr)
+StaWifiMacTestCase::RxCallback (std::string context, Ptr<const Packet> p)
 {
-  uint32_t nodeId = ContextToNodeId (context);
-  NS_LOG_DEBUG ("Node id " << nodeId << " has associated with " << bssidAddr);
-  m_assocLog.push_back (std::make_tuple (nodeId, bssidAddr));
+  Ptr<Packet> packet = p->Copy ();
+  WifiMacHeader hdr;
+  packet->RemoveHeader (hdr);
+  if (hdr.IsBeacon ())
+    {
+      Mac48Address bssid = hdr.GetAddr3 ();
+      NS_LOG_DEBUG ("Received beacon from " << bssid);
+    }
 }
 
 void
-StaWifiMacTestCase::RunStaticStaTest ()
+StaWifiMacTestCase::TurnBeaconGenerationOn (Ptr<Node> apNode)
 {
-  Ptr<YansWifiChannel> channel = CreateObject<YansWifiChannel> ();
-  ObjectFactory propDelay;
-  propDelay.SetTypeId ("ns3::ConstantSpeedPropagationDelayModel");
-  Ptr<PropagationDelayModel> propagationDelay = propDelay.Create<PropagationDelayModel> ();
-  Ptr<PropagationLossModel> propagationLoss = CreateObject<RandomPropagationLossModel> ();
-  channel->SetPropagationDelayModel (propagationDelay);
-  channel->SetPropagationLossModel (propagationLoss);
-
-  NodeContainer apNodes = NodeContainer ();
-  Ptr<ConstantPositionMobilityModel> apMobility = CreateObject<ConstantPositionMobilityModel> ();
-
-  apMobility->SetPosition (Vector (0.0, 0.0, 0.0));
-  apNodes.Add (CreateNode ("ns3::ApWifiMac", channel, apMobility, 1));
-  apMobility->SetPosition (Vector (10.0, 0.0, 0.0));
-  apNodes.Add (CreateNode ("ns3::ApWifiMac", channel, apMobility, 6));
-  apMobility->SetPosition (Vector (5.0, 5.0, 0.0));
-  apNodes.Add (CreateNode ("ns3::ApWifiMac", channel, apMobility, 11));
-
-  NodeContainer staNodes = NodeContainer ();
-  Ptr<ConstantPositionMobilityModel> staMobility = CreateObject<ConstantPositionMobilityModel> ();
-
-  staMobility->SetPosition (Vector (1.0, 0.0, 0.0));
-  staNodes.Add (CreateNode ("ns3::StaWifiMac", channel, staMobility, 1));
-  staMobility->SetPosition (Vector (10.0, 1.0, 0.0));
-  staNodes.Add (CreateNode ("ns3::StaWifiMac", channel, staMobility, 1));
-  staMobility->SetPosition (Vector (4.0, 5.0, 0.0));
-  staNodes.Add (CreateNode ("ns3::StaWifiMac", channel, staMobility, 1));
-
-  Config::Connect ("/NodeList/*/DeviceList/*/$ns3::WifiNetDevice/Mac/$ns3::StaWifiMac/Assoc", MakeCallback (&StaWifiMacTestCase::AssocCallback, this));
-
-  Simulator::Stop (Seconds (3.0));
-  Simulator::Run ();
-  Simulator::Destroy ();
-
-  //\todo: Add test assert
-}
-
-void
-StaWifiMacTestCase::SetStaVelocity (Vector vector)
-{
-  m_mobileStaMobility->SetVelocity (vector);
-}
-
-void
-StaWifiMacTestCase::RunMobileStaTest ()
-{
-  Ptr<YansWifiChannel> channel = CreateObject<YansWifiChannel> ();
-  ObjectFactory propDelay = ObjectFactory ("ns3::ConstantSpeedPropagationDelayModel");
-  Ptr<PropagationDelayModel> propagationDelay = propDelay.Create<PropagationDelayModel> ();
-  Ptr<PropagationLossModel> propagationLoss = CreateObject<RandomPropagationLossModel> ();
-  channel->SetPropagationDelayModel (propagationDelay);
-  channel->SetPropagationLossModel (propagationLoss);
-
-  NodeContainer apNodes = NodeContainer ();
-  Ptr<ConstantPositionMobilityModel> apMobility = CreateObject<ConstantPositionMobilityModel> ();
-
-  apMobility->SetPosition (Vector (0.0, 0.0, 0.0));
-  apNodes.Add (CreateNode ("ns3::ApWifiMac", channel, apMobility, 1));
-  apMobility->SetPosition (Vector (10.0, 0.0, 0.0));
-  apNodes.Add (CreateNode ("ns3::ApWifiMac", channel, apMobility, 1));
-
-  m_mobileStaMobility = CreateObject<ConstantVelocityMobilityModel> ();
-  m_mobileStaMobility->SetPosition (Vector (1.0, 0.0, 0.0));
-  Ptr<Node> staNode = CreateNode ("ns3::StaWifiMac", channel, m_mobileStaMobility, 1);
-
-  Simulator::Schedule (Seconds (1.0), &StaWifiMacTestCase::SetStaVelocity, this, Vector (2.0, 0.0, 0.0));
-  Simulator::Schedule (Seconds (1.0), &StaWifiMacTestCase::SetStaVelocity, this, Vector (0.0, 0.0, 0.0));
-
-  Config::Connect ("/NodeList/*/DeviceList/*/$ns3::WifiNetDevice/Mac/$ns3::StaWifiMac/Assoc", MakeCallback (&StaWifiMacTestCase::AssocCallback, this));
-
-  Simulator::Stop (Seconds (6.0));
-  Simulator::Run ();
-  Simulator::Destroy ();
-
-  //\todo: Add test assert
+  NS_LOG_DEBUG ("Turning beacon generation on node " << apNode->GetId ());
+  Config::Set ("/NodeList/" + std::to_string(apNode->GetId ()) + "/DeviceList/*/$ns3::WifiNetDevice/Mac/$ns3::ApWifiMac/BeaconGeneration",
+               BooleanValue (true));
 }
 
 void
 StaWifiMacTestCase::DoRun (void)
 {
-  {
-    RunStaticStaTest();
-  }
+  NodeContainer apNodes;
+  apNodes.Create (2);
 
-  {
-    RunMobileStaTest();
-  }
+  Ptr<Node> apNodeNearest = CreateObject<Node> ();
+  Ptr<Node> staNode = CreateObject<Node> ();
+
+  YansWifiPhyHelper phy = YansWifiPhyHelper::Default ();
+  YansWifiChannelHelper channel = YansWifiChannelHelper::Default ();
+  phy.SetChannel (channel.Create ());
+
+  WifiHelper wifi;
+  wifi.SetStandard (WIFI_PHY_STANDARD_80211n_2_4GHZ);
+  wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager");
+
+  WifiMacHelper mac;
+  NetDeviceContainer apDevice, apDeviceNearest;
+  mac.SetType ("ns3::ApWifiMac",
+               "BeaconGeneration", BooleanValue (true));
+  apDevice = wifi.Install (phy, mac, apNodes);
+  mac.SetType ("ns3::ApWifiMac",
+               "BeaconGeneration", BooleanValue (false));
+  apDeviceNearest = wifi.Install (phy, mac, apNodeNearest);
+
+  NetDeviceContainer staDevice;
+  mac.SetType ("ns3::StaWifiMac");
+  staDevice = wifi.Install (phy, mac, staNode);
+
+  MobilityHelper mobility;
+  Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
+  positionAlloc->Add (Vector (0.0, 0.0, 0.0));
+  positionAlloc->Add (Vector (10.0, 0.0, 0.0));
+  positionAlloc->Add (Vector (5.0, 5.0, 0.0));
+  positionAlloc->Add (Vector (5.0, 6.0, 0.0));
+  mobility.SetPositionAllocator (positionAlloc);
+
+  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+  mobility.Install (apNodes);
+  mobility.Install (apNodeNearest);
+  mobility.Install (staNode);
+
+  Config::Connect ("/NodeList/*/DeviceList/*/$ns3::WifiNetDevice/Mac/$ns3::StaWifiMac/Assoc", MakeCallback (&StaWifiMacTestCase::AssocCallback, this));
+  Config::Connect ("/NodeList/*/DeviceList/*/$ns3::WifiNetDevice/Phy/$ns3::WifiPhy/PhyRxBegin", MakeCallback (&StaWifiMacTestCase::RxCallback, this));
+
+  Simulator::Schedule (Seconds (0.05), &StaWifiMacTestCase::TurnBeaconGenerationOn, this, apNodeNearest);
+
+  Simulator::Stop (Seconds (0.2));
+  Simulator::Run ();
+  Simulator::Destroy ();
+
+  NS_TEST_ASSERT_MSG_EQ (m_associatedApBssid, Mac48Address ("00:00:00:00:00:03"), "STA associated to the wrong AP");
 }
 
 /**
