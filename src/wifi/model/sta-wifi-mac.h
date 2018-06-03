6432de36402f3afa626265c04ac5180f61801fdf
Muhammad Iqbal CR <muh.iqbal.cr@gmail.com>
2018-06-03 12:35:54 +0700

    wifi: Added BeaconArrival trace on StaWifiMac

--- src/wifi/model/sta-wifi-mac.h
+++ src/wifi/model/sta-wifi-mac.h
@@ -318,6 +318,7 @@ private:
 
   TracedCallback<Mac48Address> m_assocLogger;   ///< assoc logger
   TracedCallback<Mac48Address> m_deAssocLogger; ///< deassoc logger
+  TracedValue<Time> m_beaconArrival;            ///< beacon arrival logger
 };
 
 } //namespace ns3
